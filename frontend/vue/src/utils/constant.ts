00fc0b653e8126fd1b0ff912533f1237633c6a2f
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2023-12-28 00:47:35 +0100

    ingress

--- frontend/vue/src/utils/constant.ts
+++ frontend/vue/src/utils/constant.ts
@@ -1,2 +1,2 @@
-export const API_SERVER = "http://recette-api.groupe6.froissant.work/";
+export const API_SERVER = "http://recette.groupe6.froissant.work/api/v1/";
 export const API_PROD = process.env.VUE_APP_API_URL;
