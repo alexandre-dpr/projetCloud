a9d42323037759bbb5574657c562f20656580c64
Strike6794 <damien.crespeaule28@gmail.com>
2023-12-28 19:28:27 +0100

    [Back]essai connection mongo

--- backend/src/main/java/com/projetcloud/ProjetCloudApplication.java
+++ backend/src/main/java/com/projetcloud/ProjetCloudApplication.java
@@ -2,6 +2,9 @@ package com.projetcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -9,6 +12,13 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class ProjetCloudApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProjetCloudApplication.class, args);
+
+    }
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        // Vous pouvez personnaliser davantage la configuration ici si nécessaire
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory("mongodb+srv://projetCloud:qopjor-bygcut-5guvnY@projetcloud.kyhrlvv.mongodb.net/dbProjetCloud?retryWrites=true&w=majority"));
     }
 }
