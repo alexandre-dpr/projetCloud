b8cebb7f521f194a4a162e29fec81afe12143f7f
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2023-12-30 01:59:18 +0100

    Pipeline Deploy to Env

--- outils/terraform/variables.tf
+++ outils/terraform/variables.tf
@@ -1,4 +1,24 @@
+variable "projectName" {
+  description = "Project name"
+  default     = "ProjetCloud"
+}
+
+variable "cluster_name" {
+  description = "The name for the AKS cluster"
+  default     = "AKS"
+}
+
+variable "env_name" {
+  type        = string
+  description = "The environment for the AKS cluster"
+}
+
 variable "githubToken" {
   type        = string
   description = "GitHub token for authentication"
+}
+
+variable "helm_folder" {
+  description = "Relative path of Helm folder"
+  default     = "app/"
 }
