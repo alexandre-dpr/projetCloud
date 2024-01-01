f7784f8431b71f6145a796cfb19e81caa155a20c
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2024-01-02 00:06:50 +0100

    Ajout du certificat tls

--- outils/terraform/main.tf
+++ outils/terraform/main.tf
@@ -89,6 +89,6 @@ resource "null_resource" "helm_local" {
 
   provisioner "local-exec" {
     # Installation de l'Ingress nginx
-    command = "helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx"
+    command = "helm install ingress-controller ingress-nginx/ingress-nginx"
   }
 }
