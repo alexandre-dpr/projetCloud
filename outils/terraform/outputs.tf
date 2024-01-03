cd036f98e5eeee5b695161474c9a68f81ff7f88c
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2024-01-03 22:39:33 +0100

    --Checkpoint: Ingress OK
    On remet cert-manager

--- outils/terraform/outputs.tf
+++ outils/terraform/outputs.tf
@@ -3,3 +3,8 @@ resource "local_file" "kubeconfig" {
   filename   = "kubeconfig"
   content    = azurerm_kubernetes_cluster.cluster.kube_config_raw
 }
+
+output "ingress_ip" {
+  description = "IP de l'ingress"
+  value       = azurerm_kubernetes_cluster.cluster.kube_config[0].host
+}
