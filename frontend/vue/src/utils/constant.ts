0097a60f06b81074964a7617b2ec1e7e5daf9979
Dupre Alexandre <alexandre.dupre@etu.univ-orleans.fr>
2024-01-06 23:10:36 +0100

    Variabilisation URL api prod dans frontend

--- frontend/vue/src/utils/constant.ts
+++ frontend/vue/src/utils/constant.ts
@@ -1,2 +1,1 @@
-export const API_SERVER = "https://recette.groupe6.froissant.work/api/v1/";
-export const API_PROD = process.env.VUE_APP_API_URL;
+export const API_SERVER = process.env.VUE_APP_API_URL;
